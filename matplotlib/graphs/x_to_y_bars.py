0e1507dddcd829f7a8bab097ae0d8e286e532e6b
tendulkar <yugandhar.nanda@olacabs.com>
2017-08-06 14:49:57 +0530

    Adding bars and histogram views

---
+++ matplotlib/graphs/x_to_y_bars.py
@@ -0,0 +1,14 @@
+# bars are nothing but graphs with filling area
+# Since it's width is more, it's difficult capture intersections
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+X = np.linspace(-np.pi, np.pi, num=256, endpoint=True)
+C, S = np.cos(X), np.sin(X)
+
+plt.bar(X, C, color="blue", label="cos")
+plt.bar(X, S, color="red", label="sin")
+plt.legend()
+
+plt.show()
---
+++ matplotlib/graphs/x_to_y_histograms.py
@@ -0,0 +1,17 @@
+# histogram try to put the values in bins
+# take bin id as x, bin count as y, hence it'll help you to look into the distributions
+# so histograms are essentially distribution viewers
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+X = np.linspace(-np.pi, np.pi, num=2048, endpoint=True)
+C, S = np.cos(X), np.sin(X)
+bins = [-1.0, -0.5, 0, 0.5, 1.0]
+
+plt.hist(C, bins, color="blue", label="cos")
+# plt.hist(S, bins, color="red", label="sin")
+plt.legend()
+plt.title("distribution of cosine")
+
+plt.show()
